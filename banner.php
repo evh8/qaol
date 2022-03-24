9776efae6376098d3a26a8d445f04d04bb13a68a
plqi <101540885+plqi@users.noreply.github.com>
2022-03-23 22:51:51 -0400

    Update banner.php

--- banner.php
+++ banner.php
@@ -1,16 +1,4 @@
-<?php
-
-$dir = "static/banners/";
-$images = array();
-if ($handle = scandir($dir)) {
-        foreach ($handle as $target) {
-                if (!in_array($target, [".", ".."])) {
-                        $images[] = $target;
-                }
-        }
-}
-
-shuffle($images);
-header("Location: {$dir}/{$images[0]}");
-
-?>
+<?php                                                                                                                                                                          
+        $banners = glob('static/banners/*');                                                                                                                                   
+                                                                                                                                                                     
+        header(sprintf('Location: %s', $banners[array_rand($banners)]));
